de4e8f22901e09e3ae42409839f33ad7c9cdedf2
taufiqurSabbir <taufiqur2511@gmail.com>
2026-05-02 22:49:18 +0600

    module_10_class_2

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'module_10/class_1.dart';
+import 'module_10/class_2.dart';
 import 'module_9/class_3.dart';
 
 class MyApp extends StatelessWidget {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         title: 'Flutter 16',
-      home: Module10Class1(),
+      home: Class2(),
     );
   }
 }
---
+++ lib/module_10/class_2.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Class2 extends StatelessWidget {
+  const Class2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Appbar'),
+        backgroundColor: Colors.orange,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Hello world',style: TextStyle(fontSize: 25),),
+
+    Align(
+        alignment: Alignment.topRight,
+        child: Text('Hello world 2 ',style: TextStyle(fontSize: 25))),
+          
+          
+          Container(
+            padding: EdgeInsets.all(10), //inside
+            margin: EdgeInsets.all(10), //outside
+            alignment: Alignment.bottomCenter,
+
+            height: 150,
+            width: 150,
+
+
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [Colors.blue , Colors.purple],
+              begin: Alignment.topRight,
+                end: Alignment.bottomLeft
+
+              ),
+              border: Border.all(width: 5,color: Colors.grey),
+              borderRadius: BorderRadius.circular(10),
+              //   borderRadius: BorderRadius.only(topLeft: Radius.circular(10) , bottomRight: Radius.circular(10)),
+
+
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.red.shade200,
+                  spreadRadius: 5,
+                  offset: Offset(0, 5),
+                  blurRadius: 5
+                ),
+                BoxShadow(
+                  color: Colors.green.shade200,
+                  spreadRadius: 5,
+                  offset: Offset(5, 0),
+                  blurRadius: 5
+                )
+              ]
+            ),
+
+            child: Text('Hello flutter',style: TextStyle(fontSize: 25),),
+          )
+
+          
+        ],
+      ),
+    );
+  }
+}
